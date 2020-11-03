f79a461de4628d87cae3c47d4c27016fbccb3690
Agnija <agnijabako@gmail.com>
2020-11-03 14:13:06 +0100

    first problem set

---
+++ 01/coins/coins.c
@@ -0,0 +1,51 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+// Return a minimum number of coins to be returned as change to the user
+
+int main(void)
+{
+    //Get user input
+    float change;
+    do
+    {
+        change = get_float("Change owed:\n");
+    } while (change < 0);
+
+    //Convert dollars into cents
+    int cents = round(change * 100);
+
+    int coins = 0;
+    int newRemainder = cents;
+
+    //Count all possible 25 cents
+    while (newRemainder >= 25)
+    {
+        newRemainder = newRemainder - 25;
+        coins = coins + 1;
+    }
+
+    //Count all possible 10 cents
+    while (newRemainder >= 10)
+    {
+        newRemainder = newRemainder - 10;
+        coins = coins + 1;
+    }
+
+    //Count all possible 5 cents
+    while (newRemainder >= 5)
+    {
+        newRemainder = newRemainder - 5;
+        coins = coins + 1;
+    }
+
+    //Count all possible 1 cents
+    while (newRemainder >= 1)
+    {
+        newRemainder = newRemainder - 1;
+        coins = coins + 1;
+    }
+
+    printf("%i", coins);
+}
---
+++ 01/validate credit card/validate.c
@@ -0,0 +1,109 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Validate credit card using Luhn’s Algorithm and determine whether it's a VISA, Mastercard or Amex
+
+int main(void)
+{
+
+    //Get user input
+    long int creditCardNumber = get_long("Number:");
+    long int creditCardNumberCount = creditCardNumber;
+
+    //Find the number of digits in the creditCardNumber
+    int count = 0;
+    while (creditCardNumberCount != 0)
+    {
+        creditCardNumberCount /= 10;
+        count++;
+    }
+
+    long int remainderOne;
+    long int sumSecondDigits = 0;
+    long int x = 1;
+    double y = 0.1;
+    int secondDigit;
+    int productDigit;
+    int separatedRemainder;
+    int separatedDigit;
+    int sumProductDigit;
+
+    // Calculate the sum of every second digit in the creditCardNumber
+    for (int i = 0; i < count / 2; i++)
+    {
+        x = x * 100;
+        y = y * 100;
+
+        remainderOne = creditCardNumber % x;
+        secondDigit = remainderOne / y;
+        productDigit = secondDigit * 2;
+
+        // Separate all double digits into single and add them together
+        if (productDigit >= 10)
+        {
+            separatedRemainder = productDigit % 10;
+            separatedDigit = productDigit / 10;
+            sumProductDigit = separatedRemainder + separatedDigit;
+
+            //Calculate the sum of all second digits if there are double digits
+            sumSecondDigits = sumSecondDigits + sumProductDigit;
+        }
+        else
+        {
+            //Calculate the sum of all second digits if there are no double digits
+            sumSecondDigits = sumSecondDigits + productDigit;
+        }
+    }
+
+    double z = 0.1;
+    double a = 0.01;
+    long int remainderTwo;
+    int firstDigit;
+    int sumFirstDigits = 0;
+
+    // Calculate the sum of the remaining digits in the creditCardNumber
+    for (int i = 0; i <= count / 2; i++)
+    {
+        z = z * 100;
+        a = a * 100;
+        long int b = z;
+
+        remainderTwo = creditCardNumber % b;
+        firstDigit = remainderTwo / a;
+        sumFirstDigits = sumFirstDigits + firstDigit;
+    }
+
+    //Calculate the total sum
+    int sum;
+    sum = sumSecondDigits + sumFirstDigits;
+
+    //Get the first two or 1 digit for each type of credit card
+    int AmericanExpress = creditCardNumber / 10000000000000;
+    int MasterCard = creditCardNumber / 100000000000000;
+    int Visa13 = creditCardNumber / 1000000000000;
+    int Visa16 = creditCardNumber / 1000000000000000;
+
+    //Print INVALID if credit card number provided is not valid
+    if (sum % 10 != 0)
+    {
+        printf("INVALID\n");
+    }
+    else if (count == 15 && (AmericanExpress == 34 || AmericanExpress == 37))
+    {
+        printf("AMEX\n");
+    }
+
+    else if (count == 16 && (MasterCard > 50 && MasterCard < 56))
+    {
+        printf("MASTERCARD\n");
+    }
+
+    else if ((count == 13 || count == 16) && (Visa13 == 4 || Visa16 == 4))
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+}
